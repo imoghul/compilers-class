4a52dc947ebb10abf960501d972175f4c61025a3
imoghul <moghul02@gmail.com>
2024-03-07 17:16:46 -0500

    .

--- p2.cpp
+++ p2.cpp
@@ -292,8 +292,7 @@ static void doCSE(BasicBlock *BB, Value *I)
     if (!cseSupports((Instruction *)I))
         return;
 
-    for (auto i = BB->begin(); i != BB->end(); ++i)
-        ; // points to each instruction
+    for (auto i = BB->begin(); i != BB->end())
     {
         auto &inst = *i;
         i++;
