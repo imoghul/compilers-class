2f66d528af451b07700a6a65e683b257ae1c61db
imoghul <moghul02@gmail.com>
2024-03-07 23:19:29 -0500

    .

--- p2.cpp
+++ p2.cpp
@@ -406,6 +406,7 @@ static void CommonSubexpressionElimination(Module *M)
                 if(i->getOpcode()==Instruction::Load){
                     auto j = i;
                     j++;
+                    if(j==BB->end()) break;
                     auto& inst = *j;
                     for(;j!=BB->end();){
                         if(inst.getOpcode()==Instruction::Load && ! inst.isVolatile() && inst.getAccessType() == inst.getAccessType() && inst.getOperand(0) == inst.getOperand(0)){
