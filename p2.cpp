14af6f1f73d27b619af916f211412e555dbf9a73
imoghul <moghul02@gmail.com>
2024-03-17 16:07:37 -0400

    .

--- p2.cpp
+++ p2.cpp
@@ -589,6 +589,10 @@ static void CommonSubexpressionElimination(Module *M)
     // optimization 2
     redundantLoad(M);
     redundantStore(M);
+    
+    cse(M);
+    simplify(M);
+    removeDead(M);
 
     // optimization 3
 
