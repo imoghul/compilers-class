1a6a787119e525843916a9c476d4fc5de35ec73e
imoghul <moghul02@gmail.com>
2024-04-12 14:58:58 -0400

    fix

--- p3.cpp
+++ p3.cpp
@@ -170,47 +170,52 @@ static bool toReplicate(const Instruction &i)
 {
   switch (i.getOpcode())
   {
-  case Instruction::Alloca:
-  case Instruction::Call:
-  case Instruction::Store:
-  case Instruction::ICmp:
-  case Instruction::FCmp:
-    // branch
-    return false;
-  case Instruction::Add:
-  case Instruction::FNeg:
-  case Instruction::FAdd:
-  case Instruction::Sub:
-  case Instruction::FSub:
-  case Instruction::Mul:
-  case Instruction::FMul:
-  case Instruction::UDiv:
-  case Instruction::SDiv:
-  case Instruction::FDiv:
-  case Instruction::URem:
-  case Instruction::SRem:
-  case Instruction::FRem:
-  case Instruction::Shl:
-  case Instruction::LShr:
-  case Instruction::AShr:
-  case Instruction::And:
-  case Instruction::Or:
-  case Instruction::Xor:
-  case Instruction::GetElementPtr:
-  case Instruction::Trunc:
-  case Instruction::ZExt:
-  case Instruction::SExt:
-  case Instruction::FPToUI:
-  case Instruction::FPToSI:
-  case Instruction::UIToFP:
-  case Instruction::SIToFP:
-  case Instruction::FPTrunc:
-  case Instruction::FPExt:
-  case Instruction::PtrToInt:
-  case Instruction::IntToPtr:
-  case Instruction::BitCast:
-  case Instruction::AddrSpaceCast:
-    return true;
+    case Instruction::Alloca:
+    case Instruction::Call:
+    case Instruction::Store:
+    case Instruction::ICmp:
+    case Instruction::FCmp:
+      // branch
+      return false;
+    case Instruction::Add:
+    case Instruction::FNeg:
+    case Instruction::FAdd:
+    case Instruction::Sub:
+    case Instruction::FSub:
+    case Instruction::Mul:
+    case Instruction::FMul:
+    case Instruction::UDiv:
+    case Instruction::SDiv:
+    case Instruction::FDiv:
+    case Instruction::URem:
+    case Instruction::SRem:
+    case Instruction::FRem:
+    case Instruction::Shl:
+    case Instruction::LShr:
+    case Instruction::AShr:
+    case Instruction::And:
+    case Instruction::Or:
+    case Instruction::Xor:
+    case Instruction::GetElementPtr:
+    case Instruction::Trunc:
+    case Instruction::ZExt:
+    case Instruction::SExt:
+    case Instruction::FPToUI:
+    case Instruction::FPToSI:
+    case Instruction::UIToFP:
+    case Instruction::SIToFP:
+    case Instruction::FPTrunc:
+    case Instruction::FPExt:
+    case Instruction::PtrToInt:
+    case Instruction::IntToPtr:
+    case Instruction::BitCast:
+    case Instruction::AddrSpaceCast:
+    case Instruction::ExtractElement:
+    case Instruction::InsertElement:
+    case Instruction::ShuffleVector:
+    case Instruction::ExtractValue:
+    case Instruction::InsertValue:
+      return true;
   }
   return false;
 }
@@ -233,11 +238,11 @@ static void replicateCode(Function *F)
     }
     for (auto c = cloneMap.begin(); c != cloneMap.end(); c++)
     {
-      for (int i = 0; i < c->second->getNumOperands(); ++i)
+      for (int i = 0; i < c->first->getNumOperands(); ++i)
       {
-        if (cloneMap.find((Instruction* const)(c->second->getOperand(i))) != cloneMap.end())
+        if (cloneMap.find((Instruction* const)(c->first->getOperand(i))) != cloneMap.end())
         {
-          c->second->setOperand(i, cloneMap.find((Instruction* const)(c->second->getOperand(i)))->second );
+          c->first->setOperand(i, cloneMap.find((Instruction* const)(c->first->getOperand(i)))->second );
         }
       }
     }
